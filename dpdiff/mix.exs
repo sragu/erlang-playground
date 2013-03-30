0b5e6e87762db6bfe1c6470a6a25b4cf30cf0e34
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2013-03-30 14:14:48 +0000

    introducting riak core dependency
    needed to manually inclue all deps as there is not transitive deps.
    
    some problems with mix and riak_core
    mix not able to fetch branch dependecies for mochiweb, which failed the
    compilation of webmachine.
    
    mix needs fix? so removed the deps on webmachine.
    using similiar technique like yrashk did

--- dpdiff/mix.exs
+++ dpdiff/mix.exs
@@ -13,8 +13,23 @@ defmodule Dpdiff.Mixfile do
   end
 
   # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
+  #{ :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [{ :riak_core,	github: "basho/riak_core", tag: "1.3.0",
+					compile: "sh -c '#{root}/patches/riak_core_build.sh #{root}'"},
+		{:lager,	github: "basho/lager",	tag: "1.2.2"},
+		{:poolboy,	github: "devinus/poolboy"}, #basho poolboy is not working
+		{:protobuffs,	github: "basho/erlang_protobuffs"},
+			{:meck,	github:	"eproxus/meck"},
+		{:basho_stats,	github: "basho/basho_stats"},
+		{:riak_sysmon,	github: "basho/riak_sysmon"},
+		{:folsom,      github: "boundary/folsom"},
+			{:bear,      github: "boundary/bear", tag: "0.1.1"},
+			{:meck,      github: "eproxus/meck"}
+	]
+  end
+
+  defp root do
+	Path.dirname(__FILE__) |> Path.expand
   end
 end
