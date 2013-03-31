53d339fb013ad5c0e8a12dfc69460b0f13334a36
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2013-03-31 16:10:31 +0100

    using relex for release on simple elixir project

--- elixir_sample/mix.exs
+++ elixir_sample/mix.exs
@@ -1,3 +1,5 @@
+Code.append_path "deps/relex/ebin"
+
 defmodule ElixirSample.Mixfile do
   use Mix.Project
 
@@ -15,6 +17,18 @@ defmodule ElixirSample.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [{:relex, github: "yrashk/relex"}]
   end
+
+  if Code.ensure_loaded?(Relex.Release) do
+    defmodule Release do
+      use Relex.Release
+
+      def name, do: "rel"
+      def version, do: Mix.project[:version]
+      def applications, do: [:elixir_sample]
+      def lib_dirs, do: ["deps"]
+    end
+  end
+
 end
